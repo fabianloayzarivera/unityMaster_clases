b60e8b08859392804dcc1208c6dcdeecc7979f94
Fabian <fabianloayzarivera@gmail.com>
2017-12-14 20:13:56 +0100

    Added Navigation and Animation

---
+++ Animation/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour {
+
+	// Use this for initialization
+	Animator animator;
+	void Start () {
+		animator = GetComponent<Animator> ();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider) {
+        animator.SetBool("IsOpen", true);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
---
+++ Navigation/Assets/ArtificialIntelligence.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ArtificialIntelligence : MonoBehaviour {
+
+	NavMeshAgent agent;
+	public GameObject goal;
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		if (agent) {
+			if (goal) {
+				agent.SetDestination (goal.transform.position);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
